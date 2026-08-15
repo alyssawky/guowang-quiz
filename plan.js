12508b7222c5ed6c221f457415ecd7fca3d153cd
Keyu Wang <alyssawky@gmail.com>
2026-08-15 18:30:13 +1000

    Add study plan

---
+++ plan.js
@@ -0,0 +1,73 @@
+const studyPlan = [
+    {
+        id: "xingce-001",
+        category: "行测",
+        module: "资料分析",
+        name: "实用速算技巧",
+        week: "启动期",
+        defaultCompleted: true
+    },
+
+    {
+        id: "xingce-002",
+        category: "行测",
+        module: "判断推理",
+        name: "逻辑论证之归因论证",
+        week: "Week 1",
+        defaultCompleted: false
+    },
+
+    {
+        id: "xingce-003",
+        category: "行测",
+        module: "言语理解",
+        name: "片段阅读整体概述",
+        week: "Week 1",
+        defaultCompleted: false
+    },
+
+    {
+        id: "xingce-004",
+        category: "行测",
+        module: "数量关系",
+        name: "和差倍比与方程法",
+        week: "Week 1",
+        defaultCompleted: false
+    },
+
+    {
+        id: "xingce-005",
+        category: "行测",
+        module: "资料分析",
+        name: "高频考点之 ABRX 类",
+        week: "Week 1",
+        defaultCompleted: false
+    },
+
+    {
+        id: "computer-001",
+        category: "计算机",
+        module: "计算机基础",
+        name: "计算机系统组成",
+        week: "启动期",
+        defaultCompleted: false
+    },
+
+    {
+        id: "computer-002",
+        category: "计算机",
+        module: "数据表示",
+        name: "进位记数制 + 数制之间的相互转换",
+        week: "Week 1",
+        defaultCompleted: false
+    },
+
+    {
+        id: "computer-003",
+        category: "计算机",
+        module: "数据表示与硬件",
+        name: "机器数表示 + CPU + 冯·诺依曼体系",
+        week: "Week 1",
+        defaultCompleted: false
+    }
+];
